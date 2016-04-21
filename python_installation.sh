30dd20b6883d985c3b3fb1b80c5a6aa0cbf45106
vagrant <jongchul.seon.com>
2016-04-21 14:16:00 +0000

    image data added

--- python_installation.sh
+++ python_installation.sh
@@ -1,3 +1,7 @@
 #! /bin/sh
 
 sudo pip install -U scikit-learn seaborn future cython skdata nibabel nilearn
+
+## opencv installation
+## http://docs.opencv.org/2.4/doc/tutorials/introduction/linux_install/linux_install.html#linux-installation
+## sh -c 'ln -s /dev/null /dev/raw1394'
